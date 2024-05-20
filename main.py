6b68260155ac2013f7906d5d8da38c5d318cb849
Stormyisland <jacqueslavoie2015@gmail.com>
2024-05-19 21:24:14 -0700

    first commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 MAX_LINES =3
+MAX_BET = 21000000
+MIN_BET = 1
 
 def deposit():
     while True:
@@ -10,26 +12,51 @@ def deposit():
             else:
                 print ("Come on who you kidding cough it up. Bub!")
         else:
-            print("Give me sone money.") 
+            print("Give me some money.") 
             
     return amount    
 
+
 def get_number_of_lines():
-      while True:
+    while True:
         lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + " )? ")
         if lines.isdigit():
             lines = int(lines)
-            if 1 <=lines <= MAX_LINES:
+            if 1 <= lines <= MAX_LINES:
                 break
             else:
                 print ("Come on give me Number of lines to bet on man.")
         else:
             print("bet some lines.") 
             
-        return lines             
+        return lines
+
+def get_bet():
+    while True:
+        amount = input("How much you got to bet on each line? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print (f"Come on who you kidding cough it up. you need to enter an amount between ${MIN_BET} - ${MAX_BET} Bub!")
+        else:
+            print("Give me some money.") 
+            
+        return amount   
+            
 def main():
-   balance = deposit()
-   lines = get_number_of_lines() 
-   print(balance, lines)
+    balance = deposit()
+    lines = get_number_of_lines()
+    bet = get_bet() 
+    total_bet  = bet * lines
+    while True:
+        if total_bet > balance:
+            print(f"You dont have enough cash my man, your to broke you current balance is: ${balance}")
+    else: 
+            break
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet} ")
+
+
     
 main()               
